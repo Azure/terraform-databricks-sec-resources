3602552e041768e635e6ee9abf9b12bc7723a0e6
Mason Cusack <1masoncusack@gmail.com>
2020-06-12 17:28:03 +0100

    default incorrect

--- scripts/whls_to_dbfs.sh
+++ scripts/whls_to_dbfs.sh
@@ -7,41 +7,37 @@
 
 set -e
 
-if [ $1 == *".whl"* ]; then
+echo "Downloading custom whls for clusters..."
 
-    echo "Downloading custom whls for clusters..."
+mkdir ./defaultpackages && cd ./defaultpackages
 
-    mkdir ./defaultpackages && cd ./defaultpackages
+# First argument should be a comma-separated string of remote URIs
+IFS=', '
+read -ra ADDR <<< "$1"
+for uri in "${ADDR[@]}"; 
+do
+    # Download whl
+    curl --remote-name $uri
 
-    # First argument should be a comma-separated string of remote URIs
-    IFS=', '
-    read -ra ADDR <<< "$1"
-    for uri in "${ADDR[@]}"; 
-    do
-        # Download whl
-        curl --remote-name $uri
+    # Checksum validation
+    sha256sum $(basename $uri) > shasum.txt
+    sha256sum -c shasum.txt
+    rm shasum.txt
+done
 
-        # Checksum validation
-        sha256sum $(basename $uri) > shasum.txt
-        sha256sum -c shasum.txt
-        rm shasum.txt
-    done
+cd ..
 
-    cd ..
+echo "Downloaded. Uploading to dbfs..."
 
-    echo "Downloaded. Uploading to dbfs..."
+# dbfs auth
+export DATABRICKS_HOST=$2 # host
+export DATABRICKS_TOKEN=$3 # PAT
 
-    # dbfs auth
-    export DATABRICKS_HOST=$2 # host
-    export DATABRICKS_TOKEN=$3 # PAT
+# Upload ./defaultpackages wheelhouse to dbfs
+zip -r ./defaultpackages.wheelhouse.zip ./defaultpackages
+dbfs cp --overwrite ./defaultpackages.wheelhouse.zip dbfs:/mnt/libraries/defaultpackages.wheelhouse.zip
 
-    # Upload ./defaultpackages to dbfs
-    zip -r ./defaultpackages.wheelhouse.zip ./defaultpackages
-    dbfs cp ./defaultpackages.wheelhouse.zip dbfs:/mnt/libraries/defaultpackages.wheelhouse.zip
+echo "Uploaded!"
 
-    echo "Uploaded!"
-
-    rm -rf ./defaultpackages
-    rm -rf ./defaultpackages.wheelhouse.zip
-
-fi
+rm -rf ./defaultpackages
+rm -rf ./defaultpackages.wheelhouse.zip
