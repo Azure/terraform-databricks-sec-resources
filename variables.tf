72a3b48f90cd879db3443c71a6327b9028262d38
Luke Devonshire <luke@MacBook-Pro.local>
2020-09-03 18:24:07 +0100

    Added mounts for adls v2

--- variables.tf
+++ variables.tf
@@ -3,11 +3,32 @@ variable "databricks_workspace" {
   description = "Databricks workspace to deploy resources to."
 }
 
-variable "apim" {
-  description = "Terraform azurerm_api_management object to reference in deployment of notebook APIs."
+variable "api_management_name" {
+  type        = string
+  description = "The name of an Azure API Management instance to have the Databricks API's deployed to."
+}
+
+variable "api_management_resource_group_name" {
+  type        = string
+  description = "The name of the Azure Resource Group containing the Azure API Management instance."
+}
+
+variable "data_lake_name" {
+  description = "The name of an Azure Storage Account Datalake Storage Gen2 instance."
 }
 
 # Optional variables
+variable "prefix" {
+  type        = list(string)
+  description = "A naming prefix to be used in the creation of unique names for deployed Databricks resources."
+  default     = []
+}
+
+variable "suffix" {
+  type        = list(string)
+  description = "A naming suffix to be used in the creation of unique names for deployed Databricks resources."
+  default     = []
+}
 
 variable "notebook_path" {
   type        = string
@@ -27,20 +48,20 @@ variable "whl_upload_script_path" {
   default     = ""
 }
 
-variable "prefix" {
-  type        = list(string)
-  description = "A naming prefix to be used in the creation of unique names for deployed Databricks resources."
-  default     = []
-}
-
-variable "suffix" {
-  type        = list(string)
-  description = "A naming suffix to be used in the creation of unique names for deployed Databricks resources."
-  default     = []
-}
-
 variable "notebook_name" {
   type        = string
   description = "Desired name of the deployed notebook as it will appear in the workspace."
   default     = "mynotebook"
+}
+
+variable "libraries_container_name" {
+  type        = string
+  description = "The name of the Azure Storage Account Blob Container that will be mounted to Databricks that will contain additional libraries."
+  default     = "libraries"
+}
+
+variable "data_container_name" {
+  type        = string
+  description = "The name of the Azure Storage Account Blob Container that will be mounted to Databricks that will contain data."
+  default     = "data"
 }
