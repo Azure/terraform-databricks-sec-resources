b20fe6a6a29b81a036bea176cebaf361bc37f6da
Luke Devonshire <luke@MacBook-Pro.local>
2020-09-08 19:05:30 +0100

    Fixing args list too long

--- scripts/upload_notebook.sh
+++ scripts/upload_notebook.sh
@@ -4,22 +4,22 @@
 # $2 - The path and name to upload the notebook to in Databricks 
 # $3 - File path to Jupyter notebook
 
-set -ex
+set -e
 
+#Visual check for presence of notebook.ipynb
 pwd
-
 ls -la $3
 
 BASE64_CONTENT="$(base64 < $3)"
 
 IMPORT_PAYLOAD='{"content": '"\""$BASE64_CONTENT"\""',"path": '"\""$2"\""',"overwrite": true,"format": "JUPYTER"}'
 
-IMPORT_URL="$1/api/2.0/workspace/import/"
+echo $IMPORT_PAYLOAD > payload.json
 
-echo $IMPORT_PAYLOAD
+IMPORT_URL="$1/api/2.0/workspace/import/"
 
 echo "Uploading Notebook to $1".
 
-curl --request POST --header "Authorization:Bearer $DATABRICKS_TOKEN" --header "Content-Type:application/json" --data "$IMPORT_PAYLOAD" $IMPORT_URL
+curl --request POST --header "Authorization:Bearer $DATABRICKS_TOKEN" --header "Content-Type:application/json" --data @payload.json $IMPORT_URL
 
 echo "Finished Uploading Notebook"
